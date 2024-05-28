37893c1765e5a6ac2ec96bfc11662389ee3a2ded
sammycaetano <0000112823631xsp@al.educacao.sp.gov.br>
2024-05-28 11:18:17 -0300

    add arquivos js & css

---
+++ sketch.js
@@ -0,0 +1,22 @@
+function setup() {
+    createCanvas(400, 400);
+  }
+  
+  function draw() {
+    background(220);
+  }
+  function setup() {
+    createCanvas(600, 600);
+    background("black");
+  }
+  
+  function draw() {
+    stroke("purple");
+    fill("white");
+  
+    // console.log(mouseIsPressed)
+  
+    if (mouseIsPressed) {
+      rect(mouseX, mouseY, 20, 35);
+    }
+  }
